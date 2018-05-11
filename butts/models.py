35235356ca4055e217e8c335a3472d4a1041217c
Oscar Byrne <oscarx.john.daniel@intel.com>
2018-05-11 16:58:39 -0700

    tavs -> spaces

--- butts/models.py
+++ butts/models.py
@@ -5,6 +5,6 @@ from core.models import Asset
 
 class Butt(Asset):
 
-	def is_smelly(self):
-		print(f"Verifying 'smells' property for Butt hosted at '{self.host}' with id '{self.id}'")
-		return True
+    def is_smelly(self):
+        print(f"Verifying 'smells' property for Butt hosted at '{self.host}' with id '{self.id}'")
+        return True
